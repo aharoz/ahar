bde6f43063e4b290b0b08b0ae56afe690c6803f5
Ahmet Ozkan <65286413+aharoz@users.noreply.github.com>
2024-12-22 21:14:24 +0300

    İlk yükleme

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <h1>Merhaba</h1>
+      <h1>Merhaba!</h1>
+      <p>Benim Portfolyo Sitem</p>
+      <div>
+        <h2>Hakkımda</h2>
+        <p>Ben Ahmet Arif, web geliştirici olarak çalışıyorum.</p>
+      </div>
     </div>
   )
 } 
